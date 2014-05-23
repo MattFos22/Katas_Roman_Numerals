72295cf194b8e8d0d2ae10ed585e1b929923e846
mattfos22 <mattfoster22@gmail.com>
2014-05-23 17:11:21 +0100

    initial

---
+++ Katas_Roman_Numerals/Kata_Roman_Numerals_Tests/RomanNumerals.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kata_Roman_Numerals_Tests
+{
+    class RomanNumerals
+    {
+        public int numberOfOnes;
+        public int numberOfFives;
+        public int numberOfTens;
+        public int numberOfHundreds;
+        public string romanNumeral;
+
+        public RomanNumerals()
+        {
+
+        }
+
+        internal string GetRomanNumeral(string p)
+        {
+            ChunkNumbers(p);
+
+            if(numberOfHundreds > 0)
+            {
+                appendToString(numberOfHundreds, "C");
+            }
+            if(numberOfTens > 0)
+            {
+                appendToString(numberOfTens, "X");
+            }
+            if(numberOfFives > 0)
+            {
+                appendToString(numberOfFives, "V");
+            }
+            if(numberOfOnes > 0)
+            {
+                appendToString(numberOfOnes, "I");
+            }
+
+            return romanNumeral;
+        }
+
+        private void appendToString(int numberOfAppends, string romanCharacterToAppend)
+        {
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < numberOfAppends; i++)
+            {
+                sb.Append(romanCharacterToAppend);
+            }
+
+            romanNumeral += sb;
+        }
+
+        private void ChunkNumbers(string strNumber)
+        {
+            int originalNumber;
+            int.TryParse(strNumber, out originalNumber);
+            int tempNumber;
+
+            numberOfHundreds = originalNumber / 100;
+            numberOfTens = (originalNumber - (numberOfHundreds * 100)) / 10; 
+            tempNumber = (originalNumber - (numberOfHundreds * 100));
+            tempNumber = (tempNumber - (numberOfTens * 10));
+            numberOfFives = (tempNumber / 5);
+            tempNumber = ((numberOfFives > 0) ? 5 : 0);
+
+            numberOfOnes = tempNumber;
+        }
+    }
+}
---
+++ Katas_Roman_Numerals/Kata_Roman_Numerals_Tests/RomanNumeralsTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata_Roman_Numerals_Tests
+{
+    [TestClass]
+    public class RomanNumeralsTests
+    {
+        [TestMethod]
+        public void When_I_Enter_1_App_Returns_I()
+        {
+            RomanNumerals rn = new RomanNumerals();
+
+            string romanNumeral = rn.GetRomanNumeral("1");
+
+            Assert.AreEqual<string>("I", romanNumeral);
+        
+        }
+
+        [TestMethod]
+        public void When_I_Enter_2_App_Returns_II()
+        {
+            RomanNumerals rn = new RomanNumerals();
+
+            string romanNumeral = rn.GetRomanNumeral("2");
+
+            Assert.AreEqual<string>("II", romanNumeral);
+        }
+
+        [TestMethod]
+        public void When_I_Enter_5_App_Returns_V()
+        {
+            RomanNumerals rn = new RomanNumerals();
+
+            string romanNumeral = rn.GetRomanNumeral("5");
+
+            Assert.AreEqual<string>("V", romanNumeral);
+        }
+
+        [TestMethod]
+        public void When_I_Enter_30_App_Returns_XXX()
+        {
+            RomanNumerals rn = new RomanNumerals();
+
+            string romanNumeral = rn.GetRomanNumeral("30");
+
+            Assert.AreEqual<string>("XXX", romanNumeral);
+        }
+    }
+}
